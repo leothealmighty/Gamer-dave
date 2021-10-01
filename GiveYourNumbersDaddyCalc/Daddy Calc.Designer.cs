1068a650f372f2ad4d7073580adb500d48cc364a
Leothespicelord <leo.garcia@tooeleschools.org>
2021-10-01 09:33:46 -0600

    DadduCalu

---
+++ GiveYourNumbersDaddyCalc/Daddy Calc.Designer.cs
@@ -0,0 +1,308 @@
+﻿
+namespace GiveYourNumbersDaddyCalc
+{
+    partial class DaddyCalc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DaddyCalc));
+            this.Number1 = new System.Windows.Forms.Button();
+            this.number2 = new System.Windows.Forms.Button();
+            this.number6 = new System.Windows.Forms.Button();
+            this.number4 = new System.Windows.Forms.Button();
+            this.number3 = new System.Windows.Forms.Button();
+            this.numbrt8 = new System.Windows.Forms.Button();
+            this.number5 = new System.Windows.Forms.Button();
+            this.number0 = new System.Windows.Forms.Button();
+            this.number9 = new System.Windows.Forms.Button();
+            this.number7 = new System.Windows.Forms.Button();
+            this.add = new System.Windows.Forms.Button();
+            this.minus = new System.Windows.Forms.Button();
+            this.divide = new System.Windows.Forms.Button();
+            this.muti = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.Delete = new System.Windows.Forms.Button();
+            this.equal = new System.Windows.Forms.Button();
+            this.numberarea = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Number1
+            // 
+            this.Number1.Font = new System.Drawing.Font("Ravie", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Number1.Location = new System.Drawing.Point(91, 423);
+            this.Number1.Name = "Number1";
+            this.Number1.Size = new System.Drawing.Size(75, 75);
+            this.Number1.TabIndex = 0;
+            this.Number1.Text = "1";
+            this.Number1.UseVisualStyleBackColor = true;
+            this.Number1.Click += new System.EventHandler(this.Number1_Click);
+            // 
+            // number2
+            // 
+            this.number2.Font = new System.Drawing.Font("Ravie", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.number2.Location = new System.Drawing.Point(172, 423);
+            this.number2.Name = "number2";
+            this.number2.Size = new System.Drawing.Size(75, 75);
+            this.number2.TabIndex = 1;
+            this.number2.Text = "2";
+            this.number2.UseVisualStyleBackColor = true;
+            this.number2.Click += new System.EventHandler(this.number2_Click);
+            // 
+            // number6
+            // 
+            this.number6.Font = new System.Drawing.Font("Ravie", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.number6.Location = new System.Drawing.Point(172, 504);
+            this.number6.Name = "number6";
+            this.number6.Size = new System.Drawing.Size(75, 75);
+            this.number6.TabIndex = 2;
+            this.number6.Text = "6";
+            this.number6.UseVisualStyleBackColor = true;
+            this.number6.Click += new System.EventHandler(this.number6_Click);
+            // 
+            // number4
+            // 
+            this.number4.Font = new System.Drawing.Font("Ravie", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.number4.Location = new System.Drawing.Point(334, 423);
+            this.number4.Name = "number4";
+            this.number4.Size = new System.Drawing.Size(75, 75);
+            this.number4.TabIndex = 3;
+            this.number4.Text = "4";
+            this.number4.UseVisualStyleBackColor = true;
+            this.number4.Click += new System.EventHandler(this.number4_Click);
+            // 
+            // number3
+            // 
+            this.number3.Font = new System.Drawing.Font("Ravie", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.number3.Location = new System.Drawing.Point(253, 423);
+            this.number3.Name = "number3";
+            this.number3.Size = new System.Drawing.Size(75, 75);
+            this.number3.TabIndex = 4;
+            this.number3.Text = "3";
+            this.number3.UseVisualStyleBackColor = true;
+            this.number3.Click += new System.EventHandler(this.number3_Click);
+            // 
+            // numbrt8
+            // 
+            this.numbrt8.Font = new System.Drawing.Font("Ravie", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numbrt8.Location = new System.Drawing.Point(334, 504);
+            this.numbrt8.Name = "numbrt8";
+            this.numbrt8.Size = new System.Drawing.Size(75, 75);
+            this.numbrt8.TabIndex = 5;
+            this.numbrt8.Text = "8";
+            this.numbrt8.UseVisualStyleBackColor = true;
+            this.numbrt8.Click += new System.EventHandler(this.numbrt8_Click);
+            // 
+            // number5
+            // 
+            this.number5.Font = new System.Drawing.Font("Ravie", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.number5.Location = new System.Drawing.Point(415, 423);
+            this.number5.Name = "number5";
+            this.number5.Size = new System.Drawing.Size(75, 75);
+            this.number5.TabIndex = 6;
+            this.number5.Text = "5";
+            this.number5.UseVisualStyleBackColor = true;
+            this.number5.Click += new System.EventHandler(this.number5_Click);
+            // 
+            // number0
+            // 
+            this.number0.Font = new System.Drawing.Font("Ravie", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.number0.Location = new System.Drawing.Point(91, 504);
+            this.number0.Name = "number0";
+            this.number0.Size = new System.Drawing.Size(75, 75);
+            this.number0.TabIndex = 7;
+            this.number0.Text = "0";
+            this.number0.UseVisualStyleBackColor = true;
+            this.number0.Click += new System.EventHandler(this.number0_Click);
+            // 
+            // number9
+            // 
+            this.number9.Font = new System.Drawing.Font("Ravie", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.number9.Location = new System.Drawing.Point(415, 504);
+            this.number9.Name = "number9";
+            this.number9.Size = new System.Drawing.Size(75, 75);
+            this.number9.TabIndex = 8;
+            this.number9.Text = "9";
+            this.number9.UseVisualStyleBackColor = true;
+            this.number9.Click += new System.EventHandler(this.number9_Click);
+            // 
+            // number7
+            // 
+            this.number7.Font = new System.Drawing.Font("Ravie", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.number7.Location = new System.Drawing.Point(253, 504);
+            this.number7.Name = "number7";
+            this.number7.Size = new System.Drawing.Size(75, 75);
+            this.number7.TabIndex = 9;
+            this.number7.Text = "7";
+            this.number7.UseVisualStyleBackColor = true;
+            this.number7.Click += new System.EventHandler(this.number7_Click);
+            // 
+            // add
+            // 
+            this.add.Font = new System.Drawing.Font("Ravie", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.add.Location = new System.Drawing.Point(576, 423);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(75, 75);
+            this.add.TabIndex = 10;
+            this.add.Text = "+";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.add_Click);
+            // 
+            // minus
+            // 
+            this.minus.Font = new System.Drawing.Font("Ravie", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minus.Location = new System.Drawing.Point(495, 423);
+            this.minus.Name = "minus";
+            this.minus.Size = new System.Drawing.Size(75, 75);
+            this.minus.TabIndex = 11;
+            this.minus.Text = "-";
+            this.minus.UseVisualStyleBackColor = true;
+            this.minus.Click += new System.EventHandler(this.minus_Click);
+            // 
+            // divide
+            // 
+            this.divide.Font = new System.Drawing.Font("Ravie", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.divide.Location = new System.Drawing.Point(495, 504);
+            this.divide.Name = "divide";
+            this.divide.Size = new System.Drawing.Size(75, 75);
+            this.divide.TabIndex = 12;
+            this.divide.Text = "/";
+            this.divide.UseVisualStyleBackColor = true;
+            this.divide.Click += new System.EventHandler(this.divide_Click);
+            // 
+            // muti
+            // 
+            this.muti.Font = new System.Drawing.Font("Ravie", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.muti.Location = new System.Drawing.Point(576, 504);
+            this.muti.Name = "muti";
+            this.muti.Size = new System.Drawing.Size(75, 75);
+            this.muti.TabIndex = 13;
+            this.muti.Text = "x";
+            this.muti.UseVisualStyleBackColor = true;
+            this.muti.Click += new System.EventHandler(this.muti_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.Font = new System.Drawing.Font("Ravie", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Clear.Location = new System.Drawing.Point(10, 423);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(75, 75);
+            this.Clear.TabIndex = 14;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // Delete
+            // 
+            this.Delete.Font = new System.Drawing.Font("Ravie", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Delete.Location = new System.Drawing.Point(10, 504);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(75, 75);
+            this.Delete.TabIndex = 15;
+            this.Delete.Text = "The Sexy Button";
+            this.Delete.UseVisualStyleBackColor = true;
+            this.Delete.Click += new System.EventHandler(this.Delete_Click);
+            // 
+            // equal
+            // 
+            this.equal.Font = new System.Drawing.Font("Ravie", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.equal.Location = new System.Drawing.Point(576, 342);
+            this.equal.Name = "equal";
+            this.equal.Size = new System.Drawing.Size(75, 75);
+            this.equal.TabIndex = 16;
+            this.equal.Text = "=";
+            this.equal.UseVisualStyleBackColor = true;
+            this.equal.Click += new System.EventHandler(this.equal_Click);
+            // 
+            // numberarea
+            // 
+            this.numberarea.AutoSize = true;
+            this.numberarea.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.numberarea.Font = new System.Drawing.Font("Ravie", 39.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numberarea.Location = new System.Drawing.Point(70, 342);
+            this.numberarea.MinimumSize = new System.Drawing.Size(500, 0);
+            this.numberarea.Name = "numberarea";
+            this.numberarea.Size = new System.Drawing.Size(500, 71);
+            this.numberarea.TabIndex = 17;
+            this.numberarea.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // DaddyCalc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Purple;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1235, 583);
+            this.Controls.Add(this.numberarea);
+            this.Controls.Add(this.equal);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.muti);
+            this.Controls.Add(this.divide);
+            this.Controls.Add(this.minus);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.number7);
+            this.Controls.Add(this.number9);
+            this.Controls.Add(this.number0);
+            this.Controls.Add(this.number5);
+            this.Controls.Add(this.numbrt8);
+            this.Controls.Add(this.number3);
+            this.Controls.Add(this.number4);
+            this.Controls.Add(this.number6);
+            this.Controls.Add(this.number2);
+            this.Controls.Add(this.Number1);
+            this.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.Name = "DaddyCalc";
+            this.Text = "Daddy Calc";
+            this.Load += new System.EventHandler(this.DaddyCalc_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Number1;
+        private System.Windows.Forms.Button number2;
+        private System.Windows.Forms.Button number6;
+        private System.Windows.Forms.Button number4;
+        private System.Windows.Forms.Button number3;
+        private System.Windows.Forms.Button numbrt8;
+        private System.Windows.Forms.Button number5;
+        private System.Windows.Forms.Button number0;
+        private System.Windows.Forms.Button number9;
+        private System.Windows.Forms.Button number7;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Button minus;
+        private System.Windows.Forms.Button divide;
+        private System.Windows.Forms.Button muti;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Button Delete;
+        private System.Windows.Forms.Button equal;
+        private System.Windows.Forms.Label numberarea;
+    }
+}
+
---
+++ GiveYourNumbersDaddyCalc/Daddy Calc.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GiveYourNumbersDaddyCalc
+{
+    public partial class DaddyCalc : Form
+    {
+        String output;
+        int operand1;
+        int operand2;
+        Random rnd = new Random();
+        String opp;
+        int month;
+        public DaddyCalc()
+        {
+            InitializeComponent();
+        }
+
+        private void DaddyCalc_Load(object sender, EventArgs e)
+        {
+            output = "0";
+            numberarea.Text = "0";
+            number0.Text = "0";
+            Number1.Text = "1";
+            number2.Text = "2";
+            number3.Text = "3";
+            number4.Text = "4";
+            number5.Text = "5";
+            number6.Text = "6";
+            number7.Text = "7";
+            numbrt8.Text = "8";
+            number9.Text = "9";
+
+            operand1 = 1;
+            operand2 = 2;
+            opp = "+";
+            Console.WriteLine(1 + 2);
+            Console.WriteLine(operand1 + operand2);
+        }
+
+        private void Number1_Click(object sender, EventArgs e)
+        {
+           if(output == "0")
+            {
+                output = "7";
+            } 
+           else
+            {
+                output = output + "7";
+            }
+            
+            numberarea.Text = output;
+        }
+
+        private void number2_Click(object sender, EventArgs e)
+        {
+            
+
+            if (output == "0")
+            {
+                month = rnd.Next(1, 101);
+                output = month.ToString();
+            }
+            else
+            {
+                month = rnd.Next(1, 101);
+                output = output + month.ToString();
+            }
+
+            numberarea.Text = output;
+
+            
+        }
+
+        private void number3_Click(object sender, EventArgs e)
+        {
+
+            if (output == "0")
+            {
+                month = rnd.Next(1, 101);
+                output = month.ToString();
+            }
+            else
+            {
+                month = rnd.Next(1, 101);
+                output = output + month.ToString();
+            }
+
+            numberarea.Text = output;
+
+        }
+
+        private void number4_Click(object sender, EventArgs e)
+        {
+
+
+            if (output == "0")
+            {
+                month = rnd.Next(1, 101);
+                output = month.ToString();
+            }
+            else
+            {
+                month = rnd.Next(1, 101);
+                output = output + month.ToString();
+            }
+
+            numberarea.Text = output;
+
+        }
+
+        private void number5_Click(object sender, EventArgs e)
+        {
+
+            if (output == "0")
+            {
+                month = rnd.Next(1, 101);
+                output = month.ToString();
+            }
+            else
+            {
+                month = rnd.Next(1, 101);
+                output = output + month.ToString();
+            }
+
+            numberarea.Text = output;
+
+        }
+
+        private void number6_Click(object sender, EventArgs e)
+        {
+
+            if (output == "0")
+            {
+                month = rnd.Next(1, 101);
+                output = month.ToString();
+            }
+            else
+            {
+                month = rnd.Next(1, 101);
+                output = output + month.ToString();
+            }
+
+            numberarea.Text = output;
+
+        }
+
+        private void number7_Click(object sender, EventArgs e)
+        {
+
+
+            if (output == "0")
+            {
+                month = rnd.Next(1, 101);
+                output = month.ToString();
+            }
+            else
+            {
+                month = rnd.Next(1, 101);
+                output = output + month.ToString();
+            }
+
+            numberarea.Text = output;
+
+        }
+
+        private void numbrt8_Click(object sender, EventArgs e)
+        {
+
+            if (output == "0")
+            {
+                month = rnd.Next(1, 101);
+                output = month.ToString();
+            }
+            else
+            {
+                month = rnd.Next(1, 101);
+                output = output + month.ToString();
+            }
+
+            numberarea.Text = output;
+
+        }
+
+        private void number9_Click(object sender, EventArgs e)
+        {
+
+            if (output == "0")
+            {
+                month = rnd.Next(1, 101);
+                output = month.ToString();
+            }
+            else
+            {
+                month = rnd.Next(1, 101);
+                output = output + month.ToString();
+            }
+
+            numberarea.Text = output;
+
+        }
+
+        private void number0_Click(object sender, EventArgs e)
+        {
+
+
+            if (output == "0")
+            {
+                month = rnd.Next(1, 101);
+                output = month.ToString();
+            }
+            else
+            {
+                month = rnd.Next(1, 101);
+                output = output + month.ToString();
+            }
+
+            numberarea.Text = output;
+
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            output = "0";
+            numberarea.Text = output;
+            opp = "";
+            operand1 = 0;
+            operand2 = 0;
+        }
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            
+            operand1 = int.Parse(output);
+            opp = "+";
+            output = "0";
+            numberarea.Text = output;
+            
+        }
+
+        private void minus_Click(object sender, EventArgs e)
+        {
+            
+            operand1 = int.Parse(output);
+            opp = "-";
+            output = "0";
+            numberarea.Text = output;
+        }
+
+        private void divide_Click(object sender, EventArgs e)
+        {
+            
+            operand1 = int.Parse(output);
+            opp = "/";
+            output = "0";
+            numberarea.Text = output;
+        }
+
+        private void muti_Click(object sender, EventArgs e)
+        {
+            
+            operand1 = int.Parse(output);
+            opp = "x";
+            output = "0";
+            numberarea.Text = output;
+        }
+
+        private void equal_Click(object sender, EventArgs e)
+        {
+            int x = 0;
+            operand2 = int.Parse(output);
+
+           if (opp == "+")
+            {
+                
+
+                x = operand1 + operand2;
+                
+
+            }
+           else if(opp == "-")
+            {
+                x = operand1 - operand2;
+                
+            }
+           else if(opp == "x")
+            {
+                x = operand1 * operand2;
+                
+            }
+           else if(opp == "/")
+            {
+                x = operand1 / operand2;
+                
+            }
+            else
+            {
+                output = "0";
+            }
+            output = x.ToString();
+            numberarea.Text = output;
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            if (output == "0")
+            {
+                output = "Sexy 😏";
+            }
+            else
+            {
+                output = output + "Sexy 😏";
+            }
+
+            numberarea.Text = output;
+        }
+
+       
+    }
+}
